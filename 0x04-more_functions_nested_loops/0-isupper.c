15ef25014c182f15553844986bbb274d2e98c412
Mutimber <robinsonmutimba@gmail.com>
2022-10-21 16:58:57 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _supper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0
+*/
+int _supper(int c)
+{
+	if c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
